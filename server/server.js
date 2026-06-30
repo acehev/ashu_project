0fdd0598d3db20c16fe0ac8bc30939a4560d9eab
Ashutosh <ashutoshdeepu256@gmail.com>
2026-06-30 21:58:12 +0530

    Added database configuration and User model

--- server/server.js
+++ server/server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const dns = require('dns');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("🚀 Job Portal Backend is Running...");
 });
 
-// Port
 const PORT = process.env.PORT || 5000;
 
-// Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
