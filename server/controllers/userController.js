4735b648700b0a2cd63a0a2371f0ff5cc24f5100
Ashutosh <ashutoshdeepu256@gmail.com>
2026-07-01 11:28:24 +0530

    Implemented user registration with password hashing

---
+++ server/controllers/userController.js
@@ -0,0 +1,39 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+const hashedPassword = await bcrypt.hash(password, 10);
+    // Create new user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User Registered Successfully",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { registerUser };
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/db");
 const dns = require('dns');
 dns.setServers(['1.1.1.1', '8.8.8.8']);
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("🚀 Job Portal Backend is Running...");
